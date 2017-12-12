b79acfae5e95fa4b6ceeee0af92910c9ed028336
oprn <twros1234@gmail.com>
2017-12-12 10:59:14 +0700

    TestWhileLoop

---
+++ src/oop_lab3/TestWhileLoop.java
@@ -0,0 +1,41 @@
+package oop_lab3;
+
+public class TestWhileLoop {
+    public static void mail(String[] args){
+       // while
+            int i = 1;
+        while (i<=10) {
+            System.out.print(i + " ");
+            if (i % 10 == 0)
+                System.out.print("i+ ");
+            i++;
+        // while
+            int i = 1;
+            while (i<= 50) {
+                System.out.print(i+ " ");
+                if (i % 50 == 0)
+                    System.out.print("Hello  ");
+                i++;
+
+            }//Whil
+      //     do-While
+            System.out.print("\n");
+            int j = 1;
+            do{
+            System.out.print(j + " ");
+            j++;
+        }while ( j <=10);
+
+
+            //     do-While
+            System.out.print("\n");
+            if  (j %2 !=0 )
+            do{
+                System.out.print( "Hello ");
+                j++;
+            }while ( j <=10);
+
+
+        }//mail
+
+    }//class
