3e8e9bc67304eb2e7b8a0f5f33ef31cc01b395a6
oprn <twros1234@gmail.com>
2017-12-19 10:03:12 +0700

    TestMethods1

---
+++ src/oop_lab4/StudenData.java
@@ -0,0 +1,14 @@
+package oop_lab4;
+//สร้าง method สำหรับค่าข้อมูลนักศึกษาประกอบด้วย
+//1. ชื่อ-สกุล
+//2. ที่อยู่
+//3. รหัสนักศึกษา
+//4. สาขาและมหาวิทย่ลัย
+//5. email
+public class StudenData {
+
+    public static void main (String[] args ){
+        System.out.println("สวัสดี");
+
+}
+}//class
