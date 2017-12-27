02b1e3a1720b0b0e104cb4d0bc16e477080ca137
oprn <twros1234@gmail.com>
2017-12-27 17:38:54 +0700

    myString

--- src/oop_lab6/myString.java
+++ src/oop_lab6/myString.java
@@ -8,5 +8,23 @@ public class myString {
              String str2 = "Puriwat Lertkai";
         System.out.println(str2);
 
+
+        //String conoatenation
+        //type 1 (+)
+        String str3 = str1 + " " +str2;
+        System.out.println(str3);
+
+        //type 2 (concat() method)
+        String str4= str3.charAt("RMUTSV");
+        System.out.println(str3);
+        //String Methodes
+        System.out.println(str4.length());
+        System.out.println(str4.substring(10,20));
+        System.out.println(str4.toUpperCase());
+        System.out.println(str4.toLowerCase());
+        str4.replace('r','t');
+        str4.replace(str4.replace('r','t');
+
+
     }//main
 }//class
