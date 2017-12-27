2a75a0ef14fe18e9c3d835ab19ad2cbb14a38a74
oprn <twros1234@gmail.com>
2017-12-27 17:22:55 +0700

    InputDataToArray

--- src/oop_lab5/InputDataToArray.java
+++ src/oop_lab5/InputDataToArray.java
@@ -1,60 +1,81 @@
 package oop_lab5;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 import static oop_lab5.InputDataToArray.InputDataToArray.MAX;
 
 public class InputDataToArray {
-
    // 1.input Data from user
     // 2.find summaiion data in array
    // 3.find average value of data in array
    // 4.find maximum value of data in array
    // 5.find minimum value of data in array
-
-    public class InputDataToArray{
+    //6.Sorting data With Descending order
+    //7.Sorting data With Ascending order
+    import java.util.Arrays;
+    import java.util.concurrent;
+    import java.util.Scanner;
+    public class InputDataToArray {
         private static final int MAX = 10;
     }
         public static void  main (String[] args){
-            int num[]= new int [MAX];
+            Integer[] num= new Integer [MAX];
            num = inputData(num);
             ShowData(num);
-
+            summation(num);
+            findMax();
 
 
         }//main
 
+    private short void AscendungOrder(Integer[] num ){
+            Arrays.sort(num, Collections.reverseOrder());
+            System.out.println("Ascendung Order: ");
+            showData(num;)
+    }//AscendungOrder
 
-    public static void summation (int [] num ){
-       int total = 0;
+    private short void DescendungOrder(Integer[]num){
+            Arrays.sort(num);
+            System.out.println("Descending Order: ");
+            showData(num);
+        }//DescendingOrder
+}
+    private short void findMin(Integer[] num ){
+            System.out.println("The maximum Value is : " + (Collections.min(Arrays.asList(num))));
+    }//findMin
+    private short void findMax(Integer[] num ){
+        System.out.println("The maximum Value is : " + (Collections.max(Arrays.asList(num))));
+        //max value with basic code
+        Integer max = num[0];
+        for (int i=0;i<num.length;i++ ){
+        if (max<=num[i]){
+            max =num [i];
+        }
+        System.out.println("The maximum value is : "+max);
+    }//findMax
+    public static void summation (Integer [] num ){
+       Integer total = 0;
        for (int i = 0;i<num.length;i++){
            total +=num[i];
-
        }
 System.out.println("\nThe summation of" +"value in array is: "+total);
 System.out.println("The averag value "+ "in array is : "+total/MAX);
     }
-
-
-
-    private static int [] ShowData (int[]num) {
+    private static Integer [] ShowData (Integer []num) {
 System.out.println("Data in array : " );
 for (int val: num)
     System.out.println(val+" ");
-
     }//main
-
-
-    private static int [] inputData(int []num){
+    private static Integer [] inputData(Integer []num){
             Scanner scanner=new Scanner(System.in);
             System.out.println("Plese enter data to array: ");
             for (int i=0;i<num.length;i++){
                 System.out.println("num["+i+"]:  ");
                         num[i]= Scanner.nextInt();
-
             }
             return num;
     }
-
-
 }//class
---
+++ src/oop_lab6/myString.java
@@ -0,0 +1,12 @@
+package oop_lab6;
+
+public class myString {
+    public static  void main (String [] args ){
+        char[]c={'H','e','l','o'};
+             String str1 = new String(c);
+             System.out.println(str1);
+             String str2 = "Puriwat Lertkai";
+        System.out.println(str2);
+
+    }//main
+}//class
